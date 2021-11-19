5127607c72046b6eabc6f95b0f5e04c1b647def7
jarekjal <jarekjal@wp.pl>
2021-11-19 14:04:40 +0100

    rozwiazanie Magnets (poziom 4)

--- src/main/java/jarekjal/codewars/kyu4/Magnets.java
+++ src/main/java/jarekjal/codewars/kyu4/Magnets.java
@@ -1,10 +1,15 @@
 package jarekjal.codewars.kyu4;
 
 public class Magnets {
-    public static double doubles(int maxk, int maxn) {
-
-
-
-        return 0.0;
+    public static double doubles(int maxK, int maxN) {
+        double s = 0;
+        for (int k = 1; k <= maxK; k++) {
+            double u = 0;
+            for (int n = 1; n <= maxN; n++) {
+                u += 1.0 / (k * Math.pow(n + 1, 2 * k));
+            }
+            s += u;
+        }
+        return s;
     }
 }
